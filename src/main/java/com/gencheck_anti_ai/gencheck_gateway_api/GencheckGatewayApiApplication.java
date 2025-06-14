f3c83d40343f7193bc7d43c06a064a427ad1f829
AryanSrivastava673 <aryansrivastava673@gmail.com>
2025-06-14 14:06:18 +0530

    initial structure

---
+++ src/main/java/com/gencheck_anti_ai/gencheck_gateway_api/GencheckGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.gencheck_anti_ai.gencheck_gateway_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GencheckGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GencheckGatewayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gencheck_anti_ai/gencheck_gateway_api/GencheckGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gencheck_anti_ai.gencheck_gateway_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GencheckGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
